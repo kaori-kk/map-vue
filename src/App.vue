5835e0192bc3d901e761ec6908ae17cb332a6e31
kaori kohama <kkaori131415@gmail.com>
2020-06-23 15:45:03 +0200

    displayed map using mapbox

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Map></Map>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Map from './components/Map.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Map,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Map.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="map" style="width:700px; height:500px"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import mapboxgl, { MapboxOptions, Map } from 'mapbox-gl'
+
+@Component({})
+export default class extends Vue {
+  map: Map = {} as Map
+  option: MapboxOptions = {
+    accessToken: 'pk.eyJ1Ijoia2sta2FvcmkiLCJhIjoiY2s2YzY4d2VhMTVnYTNqcWpqcG0xaDBkMSJ9.PjJTu6RvJVQPqrT_CSUGFQ',
+    container: 'map',
+    style: 'mapbox://styles/kk-kaori/ckbrxmui90lc11ilzmyv5vwzm',
+    center: [143.767125, 38.681236],
+    zoom: 2
+  }
+
+  mounted() {
+    this.map = new mapboxgl.Map(this.option)
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+#map {
+  margin: 0 auto;
+}
+</style>
