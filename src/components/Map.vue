9f2a52592bfff2532c6c903e0cb83f2afac150cd
kaori kohama <kkaori131415@gmail.com>
2020-06-24 14:35:28 +0200

    displayed map and added search bar to fetch api

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,24 +1,53 @@
 <template>
-  <div id="map" style="width:700px; height:500px"></div>
+<div>
+  <input type="text" placeholder='Type Country Name...' v-model='query' @keypress='fetchMap'>
+  <MglMap
+    :accessToken="accessToken"
+    :mapStyle.sync ="mapStyle"
+    :center="center"
+    :zoom="zoom"
+  >
+  </MglMap>
+    <!-- <MglMarker :coordinates="coordinates" color="blue" /> -->
+</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import mapboxgl, { MapboxOptions, Map } from 'mapbox-gl'
+import { Component, Vue } from "vue-property-decorator";
+import { MglMap, MglMarker } from "vue-mapbox";
+import Mapbox from "mapbox-gl";
+// import { MarkerOptions, LngLat, LngLatLike } from "mapbox-gl";
 
-@Component({})
+@Component({
+  components: {
+    MglMap,
+    MglMarker
+  }
+})
+
 export default class extends Vue {
-  map: Map = {} as Map
-  option: MapboxOptions = {
-    accessToken: 'pk.eyJ1Ijoia2sta2FvcmkiLCJhIjoiY2s2YzY4d2VhMTVnYTNqcWpqcG0xaDBkMSJ9.PjJTu6RvJVQPqrT_CSUGFQ',
-    container: 'map',
-    style: 'mapbox://styles/kk-kaori/ckbrxmui90lc11ilzmyv5vwzm',
-    center: [143.767125, 38.681236],
-    zoom: 2
+  base_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+  query = ''
+  accessToken ='pk.eyJ1Ijoia2sta2FvcmkiLCJhIjoiY2s2YzY4d2VhMTVnYTNqcWpqcG0xaDBkMSJ9.PjJTu6RvJVQPqrT_CSUGFQ'
+  mapStyle = 'mapbox://styles/kk-kaori/ckbrxmui90lc11ilzmyv5vwzm'
+  center = [143.767125, 38.681236]
+  zoom = 2
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
   }
 
-  mounted() {
-    this.map = new mapboxgl.Map(this.option)
+  fetchMap(e){
+    if(e.key == 'Enter'){
+      fetch(`${this.base_url}${this.query}.json?limit=1&access_token=${this.accessToken}`)
+        .then(res => {
+          return res.json();
+        }).then(data => {
+          console.log(data.features[0].center)
+          //return 0 for longtitude, 1 for latitude
+        })
+    }
   }
 }
 
